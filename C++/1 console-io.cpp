1d68e9ef475e9d2d05abb5ee4dd46c0a2a68b0bd
unknown <solanki07vaishnavi@gmail.com>
2020-08-25 17:40:34 +0530

    Codes added

---
+++ C++/1 console-io.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=10,a,b;
+	double y;
+	char *ptr=“I love C++”;
+	cout<<x<<endl;
+	cout<<ptr<<endl;
+	cout<<“Enter a number”<<endl;
+	cin>>y;
+	cout<<endl;
+	cout<<x<<endl<<y<<endl<<ptr<<endl;
+	cout<<“Enter two numbers”<<endl;
+	cin>>a>>b;
+	cout<<endl<<a<<endl<<b<<endl;
+	return 0;
+}
+
